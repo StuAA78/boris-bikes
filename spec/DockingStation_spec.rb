73627d8d23e2401a9cbd0902a4977a4594430190
Stuart Adair <stu@silverka.co.uk>
2018-10-30 16:22:08 +0000

    Finished Making Docking Stations Release Bikes

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,18 @@
 require 'DockingStation'
+require 'Bike'
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
-  end 
+
+  it 'creates a new bike' do
+    station = DockingStation.new
+    expect(station.release_bike).to be_instance_of Bike
+  end
+
+  it 'bike is working' do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike.working?).to eq true
+  end
+
+end
